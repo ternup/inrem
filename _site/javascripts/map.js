63ff6b3f3f88ac0d5d77e3b549fb13b016d1f98e
Arun Kumar <arunkakorp@gmail.com>
2014-12-15 21:54:24 +0530

    Removing generated _site folder

--- _site/javascripts/map.js
+++ _site/javascripts/map.js
@@ -3,7 +3,8 @@
 
   this.data_map = {
     'South Gujarat': 'http://caddisfly.cartodb.com/api/v2/viz/c739358a-63f0-11e4-a8de-0e9d821ea90d/viz.json',
-    'North Gujarat': 'http://caddisfly.cartodb.com/api/v2/viz/49b6095c-6b26-11e4-8819-0e9d821ea90d/viz.json'
+    'North Gujarat': 'http://caddisfly.cartodb.com/api/v2/viz/49b6095c-6b26-11e4-8819-0e9d821ea90d/viz.json',
+    'Jhabua': 'http://caddisfly.cartodb.com/api/v2/viz/cdf43ca4-841f-11e4-b26a-0e0c41326911/viz.json'
   };
 
   window.onload = function() {
